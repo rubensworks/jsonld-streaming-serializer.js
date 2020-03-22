f35ba501249905d3fe8c02f40d1c71cbb002c754
Ruben Taelman <ruben.taelman@ugent.be>
2020-03-22 14:35:34 +0100

    Update spec runner to 1.1

--- spec/serializer.js
+++ spec/serializer.js
@@ -3,13 +3,13 @@ const { ErrorSkipped } = require('rdf-test-suite');
 
 module.exports = {
   serialize: function (data, baseIRI, options) {
-    if (options.processingMode && options.processingMode !== '1.0') {
+    if (options.processingMode && (options.processingMode !== '1.0' && options.processingMode !== '1.1')) {
       return Promise.reject(
         new ErrorSkipped(`Test with processing mode ${options.processingMode} was skipped, only 1.0 is supported.`));
     }
-    if (options.specVersion && options.specVersion !== '1.0') {
+    if (options.specVersion && options.specVersion !== '1.1') {
       return Promise.reject(
-        new ErrorSkipped(`Test with spec version ${options.specVersion} was skipped, only 1.0 is supported.`));
+        new ErrorSkipped(`Test with spec version ${options.specVersion} was skipped, only 1.1 is supported.`));
     }
     return require('stream-to-string')(require('streamify-array')(data)
       .pipe(new JsonLdSerializer(Object.assign({ baseIRI, space: '  ', excludeContext: true }, options))));
